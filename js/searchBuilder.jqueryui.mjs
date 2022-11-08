a69c7de8b24a49d704b2193eaf5db54b4418a012
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-08 14:11:27 +0000

    7702b412c3ac089c2c5c21039318218352233fb7 Release 1.4.0
    Sync to source repo @7702b412c3ac089c2c5c21039318218352233fb7

--- js/searchBuilder.jqueryui.mjs
+++ js/searchBuilder.jqueryui.mjs
@@ -1,11 +1,10 @@
-
 /*! JQuery ui ui integration for DataTables' SearchBuilder
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-ju';
-import DataTable from 'datatables.net-searchbuilder';
+import 'datatables.net-searchbuilder';
 
 
 $.extend(true, DataTable.SearchBuilder.classes, {
