e8f7985667bc84268f167695760ee5313ec69bdc
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-09 17:01:28 +0100

    a05244a7c2d7c499525b454760968b78dbd873ca New: Dark mode support for DataTables and Bootstrap 5.3+ styling
    Sync to source repo @a05244a7c2d7c499525b454760968b78dbd873ca

--- js/searchBuilder.jqueryui.mjs
+++ js/searchBuilder.jqueryui.mjs
@@ -2,9 +2,12 @@
  * © SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net-jqui';
 import SearchBuilder from 'datatables.net-searchbuilder';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 $.extend(true, DataTable.SearchBuilder.classes, {
     clearAll: 'ui-button ui-corner-all ui-widget dtsb-clearAll'
