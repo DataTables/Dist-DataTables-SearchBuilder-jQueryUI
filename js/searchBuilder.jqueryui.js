2100b0a7c77b89e332283d1aa0438429dd78ae21
dtbuild <dtbuild@rigil>
2026-05-07 14:00:47 +0000

    ae192b3a73e083969c7463e28f5904dc8930608f
    Sync to source repo @363f53b76e0441dbc1b2720ebe6f90cf3d008b35

--- js/searchBuilder.jqueryui.js
+++ js/searchBuilder.jqueryui.js
@@ -1,57 +1,6 @@
 /*! JQuery ui ui integration for DataTables' SearchBuilder
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-jqui', 'datatables.net-searchbuilder'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-jqui')(root, $);
-			}
-
-			if ( ! $.fn.dataTable.SearchBuilder ) {
-				require('datatables.net-searchbuilder')(root, $);
-			}
-		};
-
-		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
-					// CommonJS environments without a window global must pass a
-					// root. This will give an error otherwise
-					root = window;
-				}
-
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
-			};
-		}
-		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
-		}
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend(true, DataTable.SearchBuilder.classes, {
     clearAll: 'ui-button ui-corner-all ui-widget dtsb-clearAll'
 });
@@ -69,7 +18,3 @@ $.extend(true, DataTable.Criteria.classes, {
     right: 'ui-button ui-corner-all ui-widget dtsb-right',
     value: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-value'
 });
-
-
-return DataTable;
-}));
--- js/searchBuilder.jqueryui.mjs
+++ js/searchBuilder.jqueryui.mjs
@@ -1,27 +1,23 @@
-/*! JQuery ui ui integration for DataTables' SearchBuilder
- * © SpryMedia Ltd - datatables.net/license
+/*! SearchBuilder jQuery UI styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-jqui';
 import SearchBuilder from 'datatables.net-searchbuilder';
 
-// Allow reassignment of the $ variable
-let $ = jQuery;
-
-$.extend(true, DataTable.SearchBuilder.classes, {
+Object.assign(DataTable.SearchBuilder.classes, {
     clearAll: 'ui-button ui-corner-all ui-widget dtsb-clearAll'
 });
-$.extend(true, DataTable.Group.classes, {
+Object.assign(DataTable.Group.classes, {
     add: 'ui-button ui-corner-all ui-widget dtsb-add',
     clearGroup: 'ui-button ui-corner-all ui-widget dtsb-clearGroup',
     logic: 'ui-button ui-corner-all ui-widget dtsb-logic',
     search: 'ui-button ui-corner-all ui-widget dtsb-search'
 });
-$.extend(true, DataTable.Criteria.classes, {
+Object.assign(DataTable.Criteria.classes, {
     condition: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-condition',
     data: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-data',
-    "delete": 'ui-button ui-corner-all ui-widget dtsb-delete',
+    delete: 'ui-button ui-corner-all ui-widget dtsb-delete',
     left: 'ui-button ui-corner-all ui-widget dtsb-left',
     right: 'ui-button ui-corner-all ui-widget dtsb-right',
     value: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-value'
@@ -29,3 +25,4 @@ $.extend(true, DataTable.Criteria.classes, {
 
 
 export default DataTable;
+
