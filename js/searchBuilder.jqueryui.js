e80d7a779777598f56db2442e1d8f489efaf69e4
Allan Jardine <allan@datatables.net>
2026-05-07 15:38:36 +0000

    Updating package for DataTables 3

--- js/searchBuilder.jqueryui.js
+++ js/searchBuilder.jqueryui.js
@@ -1,20 +1,69 @@
-/*! JQuery ui ui integration for DataTables' SearchBuilder
- * © SpryMedia Ltd - datatables.net/license
+/*! SearchBuilder jQuery UI styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-$.extend(true, DataTable.SearchBuilder.classes, {
+
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-jqui', 'datatables.net-searchbuilder'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-jqui')(root);
+			}
+
+			if (! window.DataTable.SearchBuilder) {
+				require('datatables.net-searchbuilder')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+Object.assign(DataTable.SearchBuilder.classes, {
     clearAll: 'ui-button ui-corner-all ui-widget dtsb-clearAll'
 });
-$.extend(true, DataTable.Group.classes, {
+Object.assign(DataTable.Group.classes, {
     add: 'ui-button ui-corner-all ui-widget dtsb-add',
     clearGroup: 'ui-button ui-corner-all ui-widget dtsb-clearGroup',
     logic: 'ui-button ui-corner-all ui-widget dtsb-logic',
     search: 'ui-button ui-corner-all ui-widget dtsb-search'
 });
-$.extend(true, DataTable.Criteria.classes, {
+Object.assign(DataTable.Criteria.classes, {
     condition: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-condition',
     data: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-data',
-    "delete": 'ui-button ui-corner-all ui-widget dtsb-delete',
+    delete: 'ui-button ui-corner-all ui-widget dtsb-delete',
     left: 'ui-button ui-corner-all ui-widget dtsb-left',
     right: 'ui-button ui-corner-all ui-widget dtsb-right',
     value: 'ui-selectmenu-button ui-button ui-widget ui-selectmenu-button-closed ui-corner-all dtsb-value'
 });
+
+
+return DataTable;
+}));
